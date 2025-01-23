3d3222ad3375b4f57e9a30daa2d16711dd284637
jatin foujdar <jatin321001@gmail.com>
2025-01-24 02:47:48 +0530

    audio function commit

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,24 @@
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBackgroundImage = false
+    
     var body: some View {
         GeometryReader{geo in
             ZStack{
                 Image("hogwarts")
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
-                    
+                    .offset(x: moveBackgroundImage ? geo.size.width/1.1 : -geo.size.width/1.1)
+                    .onAppear{
+                        withAnimation(.linear(duration: 60).repeatForever()){
+                            moveBackgroundImage.toggle()
+                        }
+                    }
                 
                 VStack{
                     VStack{
@@ -68,6 +79,12 @@ struct ContentView: View {
                                 .cornerRadius(7)
                                 .shadow(radius: 5)
                         }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear{
+                            withAnimation(.easeInOut(duration: 1.3).repeatForever()){
+                                scalePlayButton.toggle()
+                            }
+                        }
                         Spacer()
                         
                         Button{
@@ -87,5 +104,13 @@ struct ContentView: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .preferredColorScheme(.dark)
+        
+    }
+    private func playAudio(){
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
