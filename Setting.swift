5991e1d7ecc03ba210b2c44af9049e0c5b5dd2f0
jatin foujdar <jatin321001@gmail.com>
2025-01-25 12:41:08 +0530

    setting screen design commit

--- Setting.swift
+++ Setting.swift
@@ -16,7 +16,49 @@ struct Setting: View {
                 
                 
                 ScrollView{
-                    
+                    LazyVGrid(columns: [GridItem(), GridItem()]){
+                        ZStack(alignment: .bottomTrailing){
+                            Image("hp1")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                            
+                            Image(systemName: "checkmark.circle.fill")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundColor(.green)
+                                .shadow(radius: 1)
+                                .padding(3)
+                        }
+                        ZStack(alignment: .bottomTrailing){
+                            Image("hp2")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                                .overlay(Rectangle().opacity(0.33))
+                            
+                            Image(systemName: "circle")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .foregroundColor(.green.opacity(0.5))
+                                .shadow(radius: 1)
+                                .padding(3)
+                        }
+                        ZStack{
+                            Image("hp3")
+                                .resizable()
+                                .scaledToFit()
+                                .shadow(radius: 7)
+                                .overlay(Rectangle().opacity(0.75))
+                            
+                            Image(systemName: "lock.fill")
+                                .font(.largeTitle)
+                                .imageScale(.large)
+                                .shadow(color: .white.opacity(0.75), radius: 3)
+                                .padding(3)
+                        }                        
+                    }
+                    .padding()
                 }
                 
                 Button("Done"){
