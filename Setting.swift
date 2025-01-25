fe04f7f1d66f1728cf9f5f2c4ececdea6342797f
jatin foujdar <jatin321001@gmail.com>
2025-01-25 13:40:25 +0530

    enum books commit

--- Setting.swift
+++ Setting.swift
@@ -1,8 +1,15 @@
 import SwiftUI
 
+enum BookStatus{
+    case active
+    case inactive
+    case locked
+}
+
 struct Setting: View {
     
     @Environment(\.dismiss) private var dismiss
+    @State private var books: [BookStatus] = [.active, .active, .inactive, .locked, .locked, .locked, .locked]
     
     var body: some View {
         ZStack{
@@ -17,46 +24,53 @@ struct Setting: View {
                 
                 ScrollView{
                     LazyVGrid(columns: [GridItem(), GridItem()]){
-                        ZStack(alignment: .bottomTrailing){
-                            Image("hp1")
-                                .resizable()
-                                .scaledToFit()
-                                .shadow(radius: 7)
-                            
-                            Image(systemName: "checkmark.circle.fill")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .foregroundColor(.green)
-                                .shadow(radius: 1)
-                                .padding(3)
+                        ForEach(0..<7){i in
+                            if books[i] == .active{
+                                ZStack(alignment: .bottomTrailing){
+                                    Image("hp\(i+1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                    
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundColor(.green)
+                                        .shadow(radius: 1)
+                                        .padding(3)
+                                }
+                            } else if books[i] == .inactive{
+                                
+                                ZStack(alignment: .bottomTrailing){
+                                    Image("hp\(i+1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.33))
+                                    
+                                    Image(systemName: "circle")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .foregroundColor(.green.opacity(0.5))
+                                        .shadow(radius: 1)
+                                        .padding(3)
+                                }
+                            }else {
+                                ZStack{
+                                    Image("hp\(i+1)")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .shadow(radius: 7)
+                                        .overlay(Rectangle().opacity(0.75))
+                                    
+                                    Image(systemName: "lock.fill")
+                                        .font(.largeTitle)
+                                        .imageScale(.large)
+                                        .shadow(color: .white.opacity(0.75), radius: 3)
+                                        .padding(3)
+                                }
+                            }
                         }
-                        ZStack(alignment: .bottomTrailing){
-                            Image("hp2")
-                                .resizable()
-                                .scaledToFit()
-                                .shadow(radius: 7)
-                                .overlay(Rectangle().opacity(0.33))
-                            
-                            Image(systemName: "circle")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .foregroundColor(.green.opacity(0.5))
-                                .shadow(radius: 1)
-                                .padding(3)
-                        }
-                        ZStack{
-                            Image("hp3")
-                                .resizable()
-                                .scaledToFit()
-                                .shadow(radius: 7)
-                                .overlay(Rectangle().opacity(0.75))
-                            
-                            Image(systemName: "lock.fill")
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                .shadow(color: .white.opacity(0.75), radius: 3)
-                                .padding(3)
-                        }                        
                     }
                     .padding()
                 }
